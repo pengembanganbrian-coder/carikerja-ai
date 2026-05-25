ab9123d2931c776666d41a6a94ff08bec0157d45
Bryan Shendy <pengembangan.brian@gmail.com>
2026-05-26 00:43:09 +0700

    Fix PWA manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,7 +16,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.svg",
         sizes: "512x512",
         type: "image/svg+xml",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
